8dc054bb65ae4adb4f3a579a1016cb2b8221f0b1
Qwertym <mateusz.lipowski@icloud.com>
2022-10-23 21:19:48 +0200

    poprawki

--- js/maingrafika.js
+++ js/maingrafika.js
@@ -4,12 +4,12 @@ const elem = document.getElementById('searchinput')
 function Search(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
-        window.location = 'szukaj.html#gsc.tab=0&gsc.q=' + elem.value;
+        window.location = 'szukaj=grafika.html#gsc.tab=1&gsc.q=' + elem.value;
     }
   }
 function SearchBtn(){
     if(!elem.value) return;
-    window.location = 'szukaj.html#gsc.tab=0&gsc.q=' + elem.value;
+    window.location = 'szukaj=grafika.html#gsc.tab=1&gsc.q=' + elem.value;
 
 }
 elem.addEventListener("keyup", Search); 
