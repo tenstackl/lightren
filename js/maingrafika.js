4f4f1461c6a4e042710d79cbea92eff140721af7
Tenstack <mateusz.lipowski@icloud.com>
2022-11-03 19:04:25 +0100

    poprawki

--- js/maingrafika.js
+++ js/maingrafika.js
@@ -4,18 +4,18 @@ const elem = document.getElementById('searchinput')
 function Search(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
-        window.location = 'szukaj=grafika.html#gsc.tab=1&gsc.q=' + elem.value;
+        window.location = 'search=graphics.html#gsc.tab=1&gsc.q=' + elem.value;
     }
   }
 function SearchBtn(){
     if(!elem.value) return;
-    window.location = 'szukaj=grafika.html#gsc.tab=1&gsc.q=' + elem.value;
+    window.location = 'search=graphics.html#gsc.tab=1&gsc.q=' + elem.value;
 
 }
 function SearchBtnmapy(){
   window.location = 'https://www.google.com/maps/search/' + elem.value;
 }
 function SearchBtnsiec(){
-  window.location = './szukaj.html#gsc.tab=0&gsc.q=' + elem.value;
+  window.location = './search=network.html#gsc.tab=0&gsc.q=' + elem.value;
 }
 elem.addEventListener("keyup", Search); 
